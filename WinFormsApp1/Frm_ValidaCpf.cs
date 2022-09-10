c1f98c70c059b6136aba090270fb61eba6d9d26b
RodrigoTadeu <rodrigotadeu61@gmail.com>
2022-09-10 12:19:40 -0300

    Adicionar arquivos de projeto.

---
+++ WinFormsApp1/Frm_ValidaCpf.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Frm_ValidaCpf : Form
+    {
+        public Frm_ValidaCpf()
+        {
+            InitializeComponent();
+        }
+
+        public bool Valida(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Text = "";
+           
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            string vConteudo;
+            vConteudo = Msk_CPF.Text;
+            vConteudo = vConteudo.Replace(".", "").Replace("-", "");
+            vConteudo = vConteudo.Trim();
+            if (vConteudo == "" || vConteudo.Length !=11 )
+            {
+                MessageBox.Show("Digite um CPF válido!", "Messagem de validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else
+            {
+            bool validaCpf = false;
+            validaCpf = Valida(Msk_CPF.Text);
+            if (validaCpf == true)
+            {
+               MessageBox.Show("CPF válido!","Messagem de validação",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("CPF inválido!", "Messagem de validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            }
+
+        }
+    }
+}
---
+++ WinFormsApp1/Frm_ValidaSenha.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WinFormsApp1
+{
+    partial class Fmr_ValidaSenha
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_Senha = new System.Windows.Forms.TextBox();
+            this.Lbl_Resultado = new System.Windows.Forms.Label();
+            this.Bnt_Reset = new System.Windows.Forms.Button();
+            this.Btn_VerSenha = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Txt_Senha
+            // 
+            this.Txt_Senha.Location = new System.Drawing.Point(12, 45);
+            this.Txt_Senha.Name = "Txt_Senha";
+            this.Txt_Senha.PasswordChar = '*';
+            this.Txt_Senha.Size = new System.Drawing.Size(218, 23);
+            this.Txt_Senha.TabIndex = 0;
+            this.Txt_Senha.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Txt_Senha_KeyDown);
+            // 
+            // Lbl_Resultado
+            // 
+            this.Lbl_Resultado.AutoSize = true;
+            this.Lbl_Resultado.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Lbl_Resultado.Location = new System.Drawing.Point(12, 90);
+            this.Lbl_Resultado.Name = "Lbl_Resultado";
+            this.Lbl_Resultado.Size = new System.Drawing.Size(0, 22);
+            this.Lbl_Resultado.TabIndex = 1;
+            // 
+            // Bnt_Reset
+            // 
+            this.Bnt_Reset.Location = new System.Drawing.Point(245, 42);
+            this.Bnt_Reset.Name = "Bnt_Reset";
+            this.Bnt_Reset.Size = new System.Drawing.Size(111, 23);
+            this.Bnt_Reset.TabIndex = 2;
+            this.Bnt_Reset.Text = "Limpa";
+            this.Bnt_Reset.UseVisualStyleBackColor = true;
+            this.Bnt_Reset.Click += new System.EventHandler(this.Bnt_Reset_Click);
+            // 
+            // Btn_VerSenha
+            // 
+            this.Btn_VerSenha.Location = new System.Drawing.Point(245, 71);
+            this.Btn_VerSenha.Name = "Btn_VerSenha";
+            this.Btn_VerSenha.Size = new System.Drawing.Size(111, 23);
+            this.Btn_VerSenha.TabIndex = 3;
+            this.Btn_VerSenha.Text = "Ver Senha";
+            this.Btn_VerSenha.UseVisualStyleBackColor = true;
+            this.Btn_VerSenha.Click += new System.EventHandler(this.Btn_VerSenha_Click);
+            // 
+            // Fmr_ValidaSenha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 154);
+            this.Controls.Add(this.Btn_VerSenha);
+            this.Controls.Add(this.Bnt_Reset);
+            this.Controls.Add(this.Lbl_Resultado);
+            this.Controls.Add(this.Txt_Senha);
+            this.Name = "Fmr_ValidaSenha";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Validação de Senhas";
+            this.Load += new System.EventHandler(this.Fmr_ValidaSenha_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox Txt_Senha;
+        private Label Lbl_Resultado;
+        private Button Bnt_Reset;
+        private Button Btn_VerSenha;
+    }
+}
